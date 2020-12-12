c615f6b61395b8c7dec3d12c736a8e2609b361fd
Vyron Vasileiadis <vyron.vasileiadis@atcom.gr>
2020-12-12 15:21:48 +0200

    demo

---
+++ Demo_HTTP_Pipeline/MyCustomPolicy.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Core;
+using Azure.Core.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_HTTP_Pipeline
+{
+    public class MyCustomPolicy : HttpPipelinePolicy
+    {
+        public override void Process(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            await ProcessNextAsync(message, pipeline);
+
+            stopwatch.Stop();
+            Console.WriteLine($">> Request: {message.Request.Method} {message.Request.Uri} took {stopwatch.Elapsed.TotalMilliseconds}ms");
+
+        }
+    }
+}
--- Demo_HTTP_Pipeline/Program.cs
+++ Demo_HTTP_Pipeline/Program.cs
@@ -16,7 +16,45 @@ namespace Demo_HTTP_Pipeline
     {
         static async Task Main(string[] args)
         {
-            
+            //string connectionString = Environment.GetEnvironmentVariable("ITPRODEV_STORAGE_ACCOUNT_CONNECTION_STRING");
+            BlobClientOptions options = new BlobClientOptions();
+            options.Retry.MaxRetries = 10;
+
+            options.AddPolicy(new MyCustomPolicy(), HttpPipelinePosition.PerCall);
+
+            Uri blobServiceUri = new Uri("https://itprodev2020storage.blob.core.windows.net/");
+            BlobServiceClient blobServiceclient = new BlobServiceClient(blobServiceUri, new DefaultAzureCredential(), options);
+
+            //BlobServiceClient blobServiceclient = new BlobServiceClient(connectionString, options);
+
+            BlobContainerClient blobContainerClient = blobServiceclient.GetBlobContainerClient("itprodev-container");
+
+            await blobContainerClient.CreateIfNotExistsAsync();
+
+            // Create a dummy file
+            string path = Utilities.CreateTempFile();
+
+            // Get a reference to a blob
+            BlobClient blob = blobContainerClient.GetBlobClient(Utilities.RandomName());
+
+            // Upload file to blob
+            await blob.UploadAsync(path);
+
+            // Delete local file
+            Utilities.DeleteFile(path);
+
+            await foreach (BlobItem item in blobContainerClient.GetBlobsAsync())
+            {
+                // Get a path on disk where we can download the file
+                string downloadPath = $"{item.Name}.txt";
+
+                // Get a reference to blob and download its contents
+                BlobDownloadInfo download = await blobContainerClient.GetBlobClient(item.Name).DownloadAsync();
+
+                // Copy its contents to disk
+                using FileStream file = File.OpenWrite(downloadPath);
+                await download.Content.CopyToAsync(file);
+            }
         }
     }
 }
